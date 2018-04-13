d487d7feb0a0c78624df13d2be9ef3797f3a2ba7
Matt G <mgresty223@gmail.com>
2018-04-13 19:00:48 -0400

    TriviaGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -75,3 +75,26 @@ function changeFerrel() {
     document.getElementById("body").style.backgroundImage=image;
     var q = "question" + 1;
 }
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = 0;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 1,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
