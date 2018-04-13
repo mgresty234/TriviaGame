8c3123899556dd85dad66f813be9a388207c00ab
Matt G <mgresty223@gmail.com>
2018-04-13 17:42:05 -0400

    TriviaGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@ var answers = ['will smith', 'will ferrell', 'tupac'];
 var answers1 = ['vince vaughn', 'aj holsinger', 'will ferrell', 'nipsey hustle',];
 var answers2 = ['will smith', 'will ferrel', 'austin koeppel', 'dave chapelle',];
 var answers3 = ['kevin hart', 'jack black', 'snoop dogg', 'bruce willis',];
+
 var questions = ['Who played Mike in Bad Boys 2', 'Who played in Old School', 'Who used the Cow bell on Saturday Night Live', 'Who played in Die Hard'];
 
 
@@ -10,8 +11,53 @@ var questions = ['Who played Mike in Bad Boys 2', 'Who played in Old School', 'W
 
 
 
+var total = 4;
 
 
+
+
+
+function submitAnswers() {
+    
+
+    // Get user input
+    var q1 = document.forms["quizForm"]['q1'].value;
+    var q2 = document.forms["quizForm"]['q2'].value;
+    var q3 = document.forms["quizForm"]['q3'].value;
+    var q4 = document.forms["quizForm"]['q4'].value;
+
+    // Validation
+    
+    
+
+    for (i = 1; i <= total; i++) {
+        if (eval('q'+i) == null || eval('q'+i) == ''){
+            alert('You missed question ' + i);
+            return false;
+            
+            
+        }
+    }
+    // Set answers
+    var answers = ['a', 'b', 'b', 'd'];
+    // Set score
+    var score = 0;
+
+    // Check answers
+
+    for (i = 1; i <= total; i++) {
+        if (eval('q' + i) == answers[i - 1]){
+            score++
+    
+        }
+    }
+    // Display results
+    
+    alert('You scored ' +score+' out of ' +total);
+
+    return false;
+
+}
 
 
 
@@ -21,10 +67,11 @@ var arr = ["url('http://www.lyhyxx.com/data/out/46/4156953-bruce-willis-wallpape
 "url('https://www.theplace2.ru/archive/will_ferrell/img/Will_Ferrell_Casa_de.jpg')",
 "url('http://getwallpapers.com/wallpaper/full/8/d/a/132524.jpg')",
 "url('https://images6.alphacoders.com/649/thumb-1920-649500.jpg')"];
+
+
 function changeFerrel() {
     var image= arr[Math.floor(Math.random()*arr.length)];
-    console.log(arr);
-    console.log(document.getElementById("body"))
+    
     document.getElementById("body").style.backgroundImage=image;
+    var q = "question" + 1;
 }
-
